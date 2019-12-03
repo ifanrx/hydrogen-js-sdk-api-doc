d73717fec1055857927171e22a9b80d7b3a6e66b
zhangzeshuan <zhangzeshuan@ifanr.com>
2019-12-03 14:49:44 +0800

    Add deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# init
+git submodule update --init --recursive
+cd sdk
+git checkout master
+yarn
+
+# build
+npm run gen-docs-wechat
+
+# depoly
+cd ../
+npm run gh-pages -- -d sdk/docs
